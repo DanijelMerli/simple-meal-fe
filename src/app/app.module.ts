44893d3d37b87f2c015d5ef2dea6c1d6fbcd8bbf
KristinaAndrijin <96635332+KristinaAndrijin@users.noreply.github.com>
2024-03-02 19:02:45 +0100

    side navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 // material
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from  '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // components
 import { HomePageComponent } from './components/home-page/home-page.component';
@@ -26,7 +29,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AppRoutingModule,
     MatIconModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from  '@angular/material/sidenav';
 import { JwtService } from '../../jwt/jwt.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -14,8 +15,30 @@ export class NavbarComponent implements OnInit {
 
   isTokenPresent: boolean = false;
   isLogin: boolean = false;
+  isHome: boolean = false;
 
-  constructor(private router: Router, private jwtService: JwtService) {}
+  constructor(private router: Router, private jwtService: JwtService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        
+        // Gledamo na kojoj smo putanji i prikazujemo navbar u skladu sa tim
+        const navigationEndEvent = event as NavigationEnd;
+        const url = navigationEndEvent.url;
+        console.log(url);
+        if (url.includes('login')) {
+          this.isLogin = true;
+        } else {
+          this.isLogin = false;
+        }
+
+        if (url.includes('home') || url === "/") {
+          this.isHome = true;
+        } else {
+          this.isHome = false;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     let token = this.jwtService.getToken();
