39e23d40e10e1d39011f8f5108e18b45c4cce4bb
BojanaZekanovic9 <bojana.zekanovic9@gmail.com>
2024-03-04 01:19:24 +0100

    Added login user functionalities

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup; 
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]], 
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm && this.loginForm.valid) {
+      alert("Login successful")
+      console.log(this.loginForm.value);
+    }
+  }
+
+  isFieldInvalid(field: string) {
+    const formControl = this.loginForm.get(field);
+    return formControl?.invalid && formControl?.touched;
+  }
 }
