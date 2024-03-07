68f8e3e111a3392a4d39f4766ac9c188629c6404
stx <grobari.pfc3@gmail.com>
2024-03-07 22:47:44 +0100

    error msg

--- src/app/components/registration/service/user.service.ts
+++ src/app/components/registration/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserDTO } from '../dto/UserDTO';
@@ -8,10 +8,15 @@ import { UserDTO } from '../dto/UserDTO';
 })
 export class UserService {
   private url ='http://localhost:8080';
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private http: HttpClient) { }
 
   public registerUser(user: UserDTO): Observable<UserDTO> {
-   return this.http.post<UserDTO>(`${this.url}/register`,user);
+   return this.http.post<UserDTO>(`${this.url}/register`,user,this.httpOptions);
   }
 }
