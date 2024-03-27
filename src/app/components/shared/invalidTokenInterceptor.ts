7257926e2e0c81e44489b1bf42474b57acd09717
KristinaAndrijin <96635332+KristinaAndrijin@users.noreply.github.com>
2024-03-27 20:29:46 +0100

    update interceptor

--- src/app/components/shared/invalidTokenInterceptor.ts
+++ src/app/components/shared/invalidTokenInterceptor.ts
@@ -8,14 +8,14 @@ import { UserService } from '../../services/user.service';
 
 @Injectable()
 export class InvalidTokenInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService, private userService: UserService, private router: Router) {}
+  constructor(private auth: AuthService, private userService: UserService, private router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error) => {
-        if (error.status === 403) {
+        if (error.status === 403 || error.status === 401) {
           this.userService.logOut();
-          this.router.navigate(['/login']);
+          this.router.navigate(['/login']).then(() => { location.reload(); });
         }
         return throwError(error);
       })
